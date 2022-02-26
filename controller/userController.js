a4e99e7c2963254aeb065cba3529b4a40cb49032
Jesus <jesusbalandra@outlook.com>
2022-02-26 12:49:13 -0700

    add get user by ID

--- controller/userController.js
+++ controller/userController.js
@@ -21,8 +21,17 @@ module.exports = {
     },
     getAllUsers: async (req, res) => {
         try {
-            const users = await User.find();
+            const users = await User.find({}, '-role -powerLevel');
             res.json(users);
+        } catch (e) {
+            res.json(e);
+        }
+    },
+    getUserById: async (req, res) => {
+        const { userId } = req.params;
+        try {
+            const user = await User.findById(userId, '-role -powerLevel');
+            res.json(user);
         } catch (e) {
             res.json(e);
         }
--- routes/api/user/index.js
+++ routes/api/user/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
-const { createUser, getAllUsers } = require('../../../controller/userController');
+const { createUser, getAllUsers, getUserById } = require('../../../controller/userController');
 
 router.post('/', createUser);
 router.get('/', getAllUsers);
+
+router.route('/:userId')
+    .get(getUserById)
 module.exports = router;
